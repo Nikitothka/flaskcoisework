663b69ffeb75a8a81483a88b745b838e4a5c8794
Nikita Gulyaev <71905230+Nikitothka@users.noreply.github.com>
2023-05-18 04:14:58 +0300

    Добавил сокет + задержка

--- static/script.js
+++ static/script.js
@@ -1,44 +1,44 @@
 window.onload = function () {
-    // Загрузка сохраненных работ из localStorage
-    const works = JSON.parse(localStorage.getItem("works")) || [];
-    const selectElement = document.getElementById("work-select");
-    works.forEach(work => {
-        const optionElement = document.createElement("option");
-        optionElement.value = work;
-        optionElement.textContent = work;
-        selectElement.appendChild(optionElement);
-    });
+  // Загрузка сохраненных работ из localStorage
+  const works = JSON.parse(localStorage.getItem("works")) || [];
+  const selectElement = document.getElementById("work-select");
+  works.forEach(work => {
+    const optionElement = document.createElement("option");
+    optionElement.value = work;
+    optionElement.textContent = work;
+    selectElement.appendChild(optionElement);
+  });
 
-    const fileInput = document.getElementById("file-upload");
-    const newWorkCheckbox = document.getElementById("new-work-checkbox");
-    const newWorkNameInput = document.getElementById("new-work-name");
-    const submitButton = document.getElementById("submit-button");
-    const form = document.getElementById("upload-form");
-    const loaderContainer = document.getElementById("loader-container");
-    const loaderPercentage = document.getElementById("loader-percentage");
-    const resultContainer = document.getElementById("result-container");
+  const fileInput = document.getElementById("file-upload");
+  const newWorkCheckbox = document.getElementById("new-work-checkbox");
+  const newWorkNameInput = document.getElementById("new-work-name");
+  const submitButton = document.getElementById("submit-button");
+  const form = document.getElementById("upload-form");
+  const loaderContainer = document.getElementById("loader-container");
+  const loaderPercentage = document.getElementById("loader-percentage");
+  const resultContainer = document.getElementById("result-container");
 
-    newWorkCheckbox.onchange = function () {
-        if (newWorkCheckbox.checked) {
-            selectElement.style.display = "none";
-            newWorkNameInput.style.display = "block";
-            newWorkNameInput.disabled = false;
-        } else {
-            selectElement.style.display = "block";
-            newWorkNameInput.style.display = "none";
-            newWorkNameInput.disabled = true;
-        }
-    };
+  newWorkCheckbox.onchange = function () {
+    if (newWorkCheckbox.checked) {
+      selectElement.style.display = "none";
+      newWorkNameInput.style.display = "block";
+      newWorkNameInput.disabled = false;
+    } else {
+      selectElement.style.display = "block";
+      newWorkNameInput.style.display = "none";
+      newWorkNameInput.disabled = true;
+    }
+  };
 
-    submitButton.onclick = function () {
+  submitButton.onclick = function () {
     if (fileInput.files.length === 0) {
-        alert("Пожалуйста, выберите файл для загрузки.");
-        return;
+      alert("Пожалуйста, выберите файл для загрузки.");
+      return;
     }
 
     if (!newWorkCheckbox.checked && selectElement.value === "") {
-        alert("Пожалуйста, выберите работу или создайте новую.");
-        return;
+      alert("Пожалуйста, выберите работу или создайте новую.");
+      return;
     }
 
     const formData = new FormData(form);
@@ -51,30 +51,34 @@ window.onload = function () {
     const xhr = new XMLHttpRequest();
     xhr.open(form.method, form.action);
     xhr.upload.onprogress = function (event) {
-        if (event.lengthComputable) {
-            const progressPercentage = (event.loaded / event.total) * 100;
-            loaderPercentage.textContent = `${Math.round(progressPercentage)}%`;
-        }
+      if (event.lengthComputable) {
+        const progressPercentage = (event.loaded / event.total) * 100;
+
+        // Задержка в обновлении прогресса загрузки
+        setTimeout(() => {
+          loaderPercentage.textContent = `${Math.round(progressPercentage)}%`;
+        }, 500); // Задержка в 500 миллисекунд
+      }
     };
     xhr.onload = function () {
-        loaderContainer.style.display = "none";
-        resultContainer.innerHTML = xhr.responseText;
+      loaderContainer.style.display = "none";
+      resultContainer.innerHTML = xhr.responseText;
 
-        const workName = newWorkCheckbox.checked ? newWorkNameInput.value : selectElement.value;
-        if (works.indexOf(workName) === -1) {
-            works.push(workName);
-            localStorage.setItem("works", JSON.stringify(works));
-            const optionElement = document.createElement("option");
-            optionElement.value = workName;
-            optionElement.textContent = workName;
-            selectElement.appendChild(optionElement);
-        }
+      const workName = newWorkCheckbox.checked ? newWorkNameInput.value : selectElement.value;
+      if (works.indexOf(workName) === -1) {
+        works.push(workName);
+        localStorage.setItem("works", JSON.stringify(works));
+        const optionElement = document.createElement("option");
+        optionElement.value = workName;
+        optionElement.textContent = workName;
+        selectElement.appendChild(optionElement);
+      }
     };
     xhr.onerror = function () {
-        loaderContainer.style.display = "none";
-        console.error('Error:', xhr.statusText);
-        resultContainer.innerHTML = "Произошла ошибка при отправке данных на сервер.";
+      loaderContainer.style.display = "none";
+      console.error('Error:', xhr.statusText);
+      resultContainer.innerHTML = "Произошла ошибка при отправке данных на сервер.";
     };
     xhr.send(formData);
-    };
+  };
 };
